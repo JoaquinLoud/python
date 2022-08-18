d6d3242817e46242b2ac21638ac6c7e53e3b58ab
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-18 07:21:53 -0500

    hice un commit

---
+++ actividad2/ejercicio3.py
@@ -0,0 +1,1 @@
+Lista_uno = [ 17 , 8 , 2022 ,"Grupo e.bases de datos" , True  ,["programa" , "ingenieria de sistemas" , "semestre" , 9]  ] 
---
+++ actividad2/ejercicio4.py
@@ -0,0 +1,15 @@
+listauno=[]
+terminar=True
+while(terminar):
+    nombre = input("Ingrese el Nombre = ")
+    apellido = input("Ingrese el Apellido = ")
+    edad = int(input("Ingrese la Edad = "))
+    listauno.append([nombre, apellido, edad])
+    t = input("Desea Continuar S/N =")
+    if(t == "n"):
+        terminar = False
+print(listauno)
+listados=([nombre,apellido,edad])
+listados.reverse()
+print(listados)
+
